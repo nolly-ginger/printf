a1ee93d0b10de7751baa2c9393df9530fce6935d
Nq-glith <nxobile.mahlangu@gmail.com>
2023-05-29 20:37:56 -0700

    print_data functions

---
+++ print_data.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_char - handles printing of as ingle char
+ * @c: char typs
+ * @buffer: array to handle printing
+ * @flags: indicate formatting options
+ * @width: width specifier
+ * @precision: precision specifier
+ * size: size specifier
+ * Return: number of chars printed
+ */
+int print_char(char c, char buffer[], int flags, int width,
+		int precision, int size)
+{
+}
+
+/**
+ * print_number - handles the printing of a number (int value)
+ * @is_negative: indicator of whether the number is negative
+ * @index: indicates where the number starts in the buffer
+ * @buffer: array to store number and padding
+ * @flags: indicate formatting options
+ * @width: width specifier
+ * @precision: precision specifier
+ * @size: size specifier
+ * Return: number of charecters printed
+ */
+int print_number(int is_negative, int index, char buffer[],
+		int flags, int width, int precision, int size)
+{
+}
+
+/**
+ * print_unsigned_number - prints an unsigned number value
+ */
+int print_unsigned_number(int index, char buffer[], int flags,
+		int width, int precision, int length, char padd,
+		char extra_chars
+
+
+
+
+
